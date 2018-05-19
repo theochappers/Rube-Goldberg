e3b4cf1ee49932b4742b80c80c1201e84073d4f5
Theo Chapman (s5069264) <s5069264@bournemouth.ac.uk>
2018-05-19 22:17:51 +0100

    newsave

---
+++ Assets/WindyBoiz.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindyBoiz : MonoBehaviour {
+    public float launcher;
+        private void OnTriggerStay (Collider other)
+    {
+        other.GetComponent<Rigidbody>().AddForce(Vector3.up * launcher, ForceMode.Acceleration);
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
